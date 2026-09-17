c8a5f96cee3ed4286fcea08b4784bdf5c76496b8
Aaliyan <135755847+aaliyann-exe@users.noreply.github.com>
2026-09-17 19:25:40 +0500

    lab 2 changes

---
+++ Lab02/string_methods.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+
+    string name = "aaliyan";
+
+    cout << name.length();
+    cout << name.substr(2,5);
+    cout << name.find('l');
+
+    return 0;
+
+}   
